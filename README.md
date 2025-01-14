aaf7abeafc034bff4663b77fefdb6541445330e9
MatevzTrojar <trojarmatevz@gmail.com>
2025-01-14 22:54:14 +0100

    dodal Keyboard events.

--- README.md
+++
@@ -1,2 +0,0 @@
-# igrca-za-RPAv
-To je moja igrica za RPAv. Upam da bo pozitivna ocena.
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,41 @@
 #include <SDL.h>
+#include <SDL_events.h>
+using namespace std;
+#include <iostream>
+#include <SDL_render.h>
 #include <cstdlib>
 
 int main(int argumentCount, char * arguments[])
 {
-    SDL_Window * window = SDL_CreateWindow("SDL!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 400, SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI);
+  SDL_Window * window = SDL_CreateWindow("Igrica", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1400, 700, SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,-1,0);
+    SDL_SetRenderDrawColor(renderer , 255 , 0,0,255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Event event;
+    
+    while(true)
+    {
+        if(SDL_PollEvent(&event)){
+            if(SDL_QUIT == event.type){
+                break;
+            }
+            if(SDL_KEYDOWN== event.type){
+                //cout<<"key is down"<<endl;
+                if(SDLK_a == event.key.keysym.sym)
+                    cout<<"a is down - move left"<<endl;//tuki je WASD detection
+                else  if(SDLK_s == event.key.keysym.sym)
+                    cout<<"s is down - move back"<<endl;
+                else if(SDLK_w == event.key.keysym.sym)
+                    cout<<"w is down - move up"<<endl;
+                else  if(SDLK_d == event.key.keysym.sym)
+                    cout<<"d down - move right"<<endl;
 
-    SDL_Delay(1000);
-
-    SDL_DestroyWindow(window);
-
-    return EXIT_SUCCESS;
+            }
+            else if(SDL_KEYUP== event.type){
+              //  cout<<"key is up"<<endl;
+            }
+        }
+    }
+      return EXIT_SUCCESS;
 }
